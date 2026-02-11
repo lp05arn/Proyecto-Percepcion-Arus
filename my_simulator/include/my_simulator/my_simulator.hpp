2599c1ec71d8d9275b88826e0766db5a6e7acd57
lp05arn <ruiz.navarrete.alejandro@gmail.com>
2026-02-11 10:23:13 +0100

    Simulador funcionando con TF. Utilizando RViz recibe mensajes y de velocidad y giro

--- my_simulator/include/my_simulator/my_simulator.hpp
+++ my_simulator/include/my_simulator/my_simulator.hpp
@@ -1,7 +1,12 @@
 #ifndef MY_SIMULATOR_HPP
 #define MY_SIMULATOR_HPP
 
+// Mensaje personalizado y librerías de posición
 #include "rclcpp/rclcpp.hpp"
+#include "my_simulator/msg/control.hpp"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2/LinearMath/Quaternion.h"
+#include "geometry_msgs/msg/transform_stamped.hpp"
 
 // Clase que hereda de rclcpp::Node
 class MySimulator : public rclcpp::Node
@@ -11,13 +16,27 @@ public:
     MySimulator();
 
 private:
-    // Variables de estado del coche (según el ejercicio)
+    // Variables de estado del coche
     double x_;
     double y_;
-    double yaw_;      // Orientación
-    double vx_;       // Velocidad en X
-    double vy_;       // Velocidad en Y
-    double yaw_rate_; // Velocidad de giro
+    double yaw_;    // Orientación
+    double v_;  // (m/s )
+    double L_;  // Distancia entre los ejes
+
+    // Input del usuario
+    double acc_;    // Aceleracion
+    double delta_;  // Ángulo volante
+
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    // Suscriptor
+    rclcpp::Subscription<my_simulator::msg::Control>::SharedPtr control_sub_;
+
+    // Mensaje a RViz
+    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+
+    void update_physics();
+    void control_callback(const my_simulator::msg::Control::SharedPtr msg);
 };
 
-#endif // MY_SIMULATOR_HPP
+#endif
--- my_simulator/src/my_simulator.cpp
+++ my_simulator/src/my_simulator.cpp
@@ -1,12 +1,63 @@
 #include "my_simulator/my_simulator.hpp"
 
+using namespace std::chrono_literals;
+
 MySimulator::MySimulator()
-: Node("my_simulator_node"), // Nombre del nodo en ROS
+: Node("my_simulator_node"), 
   x_(0.0), y_(0.0), yaw_(0.0),
-  vx_(0.0), vy_(0.0), yaw_rate_(0.0)
+  v_(0.0), acc_(0.0), delta_(0.0)
 {
-    // Mensaje para saber que ha arrancado
-    RCLCPP_INFO(this->get_logger(), "¡Simulador MySimulator iniciado correctamente!");
+    L_ = 2.5; // Distancia entre los ejes de 2.5 metros.
+
+    // Inicializamos el suscriptor
+    control_sub_ = this->create_subscription<my_simulator::msg::Control>(
+      "/car_control", 10, std::bind(&MySimulator::control_callback, this, std::placeholders::_1)
+    );
     
-    // Aquí inicializaremos más cosas en el futuro
+    // Inicializamos RViz y timer
+    tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+    timer_ = this->create_wall_timer(
+      100ms, std::bind(&MySimulator::update_physics, this)
+    );
+
+    // Mensaje de "arranque"
+    RCLCPP_INFO(this->get_logger(), "¡Simulador MySimulator iniciado correctamente!");
+}
+
+// Guardamos los inputs del ususario
+void MySimulator::control_callback(const my_simulator::msg::Control::SharedPtr msg)
+{
+  acc_ = msg->acc;
+  delta_ = msg->delta;
+}
+
+// Calcular físicas
+void MySimulator::update_physics()
+{
+  double dt = 0.1;
+
+  v_ += acc_*dt;
+  x_ += (v_ * cos(yaw_))*dt;
+  y_ += (v_ * sin(yaw_))*dt;
+  yaw_ += (v_ / L_) * tan(delta_)*dt;
+
+  // Lo pasamos RViz
+  geometry_msgs::msg::TransformStamped t;
+  t.header.stamp = this->get_clock()->now();
+  t.header.frame_id = "map";
+  t.child_frame_id = "base_link";
+
+  t.transform.translation.x = x_;
+  t.transform.translation.y = y_;
+  t.transform.translation.z = 0.0;
+
+  tf2::Quaternion q;
+  q.setRPY(0, 0, yaw_);
+  t.transform.rotation.x = q.x();
+  t.transform.rotation.y = q.y();
+  t.transform.rotation.z = q.z();
+  t.transform.rotation.w = q.w();
+
+  // Enviamos el "mensaje"
+  tf_broadcaster_->sendTransform(t);
 }
